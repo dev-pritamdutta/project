f0dea78a007a5dd43651f70972fc36ed4aa30876
pritam dutta <webdevpritam7498@gmail.com>
2024-12-29 00:20:04 +0600

    dfd

---
+++ foodi-client/src/components/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingSpinner = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <span className="loading loading-spinner text-secondary w-20"></span>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
---
+++ foodi-client/src/components/Offer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Offer = () => {
+  return (
+    <div className="max-w-screen-2xl container mx-auto xl:px-0 px-0">
+      {/* Full-page image */}
+      <div className="relative">
+        <img
+          src="https://i.ibb.co/mRvNysz/offer-food.jpg"
+          alt="Offer Food"
+          className="w-full h-100vh object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Offer;
